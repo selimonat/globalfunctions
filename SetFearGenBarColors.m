c93f5f5db4c00b85747253e70cad9d9eadf4dcda
onatselim <onatselim@gmail.com>
2015-06-10 16:54:20 +0200

    new fun

---
+++ SetFearGenBarColors.m
@@ -0,0 +1,24 @@
+function SetFearGenBarColors(h,varargin)
+%SetFearGenBarColors(h,vargargin)
+%
+%   Changes color of the bars to circular HSV colorset. VARARGIN can be
+%   used to set the colors to a specific value. These should RGB values for
+%   each bar, that is nBar x 3 matrix.
+%
+%   See also: SetFearGenColors.
+
+
+
+h     = get(h,'children');
+tbar  = length(get(h,'YData'));
+set(h,'CData', repmat(1:tbar,1,tbar/tbar),'edgecolor','none');
+%
+if nargin == 1
+    color    = GetFearGenColors;
+    color    = color + 20/255;
+    color    = min(color,ones(size(color)));
+    colormap(color(1:8,:));
+else
+    color    = varargin{1}';
+    colormap(color);
+end
