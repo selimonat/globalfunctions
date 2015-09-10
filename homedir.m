90376f2f77685761ff78312fad22b444596ce3cb
onatselim <onatselim@gmail.com>
2015-09-10 16:26:12 +0200

    new file

---
+++ homedir.m
@@ -0,0 +1,16 @@
+function userDir = homedir
+%homedir return the user home directory.
+%   USERDIR = homedir returns the user home directory using the registry
+%   on windows systems and using Java on non windows systems as a string
+%
+%   Example:
+%      homedir() returns on windows
+%           C:\Documents and Settings\MyName\Eigene Dateien
+
+if ispc
+    userDir = winqueryreg('HKEY_CURRENT_USER',...
+        ['Software\Microsoft\Windows\CurrentVersion\' ...
+         'Explorer\Shell Folders'],'Personal');
+else
+    userDir = char(java.lang.System.getProperty('user.home'));
+end
