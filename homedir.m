5005c3df5089b60f1c6a56eb480a5f9dbfefd899
leakp <lea.kampermann@gmail.com>
2015-09-11 14:58:16 +0200

    corrected

--- homedir.m
+++ homedir.m
@@ -11,7 +11,7 @@ if ispc
     userDir = winqueryreg('HKEY_CURRENT_USER',...
         ['Software\Microsoft\Windows\CurrentVersion\' ...
          'Explorer\Shell Folders'],'Personal');
-     regexprep(userDir,'Documents','');
+     userDir = regexprep(userDir,'Documents','');
 else
     userDir = char(java.lang.System.getProperty('user.home'));
 end
