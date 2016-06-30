31dac22f91225ac246ed1af3a118005439dd3389
onatselim <onatselim@gmail.com>
2016-06-30 18:15:11 +0200

    improvmenets

--- fitcorrmat.m
+++ fitcorrmat.m
@@ -1,79 +1,33 @@
-function [fit,param]=fitcorrmat(cormat,method)
+function [fit,param]=fitcorrmat(cormat)
 %[fit,param]=fitcorrmat(cormat)
 %
-% Will model a covariance matrix's parameters using a grid search
-% approach (default method). This function works together with getcorrmat,
-% it knows their parameters. The alternative method uses gradient descent
-% (method = 'gradient').
+% Will model a covariance matrix's parameters using gradient optimization
+% approach. This function works together with getcorrmat,
+% it knows their parameters. Initial parameters are randomly set within the
+% range of cormat.
 %
-% PARAM is [amp_circ amp_gau amp_const amp_diagonal].
+%
+% PARAM(1:4) is [amp_circ amp_gau amp_const amp_diagonal].
+%
 %
 % See also: getcorrmat
 
 
-if nargin == 1
-    method == 'grid'
-end
+viz      = 0;%see the results or not
+opt      = optimset('display','none','tolfun',10^-20,'tolx',10^-20,'maxfunevals',10000,'maxiter',10000);
+Y        = cormat;
 
-if strcmp(method,'grid');
-    r = range(cormat(:));
-    ts = size(cormat,3);%total subjects
-    res        = 50;
-    amp_circs  = linspace(0,r*2,res);
-    amp_gaus   = linspace(0,r*2,res);
-    amp_consts = linspace(0,r*2,res);
-    amp_diags  = linspace(0,r*2,res);
-    error      = nan(ts,res.^4);
-    lut        = nan(4,res.^4);
-    c          = 0;
-    for amp_circ = amp_circs
-        fprintf('%4.4g percent finished.\n',amp_circ./max(amp_circs)*100);
-        for amp_gau = amp_gaus
-            for amp_const = amp_consts
-                for amp_diag  = amp_diags
-                    c        = c + 1;
-                    for ns = 1:size(cormat,3)
-                        lut(:,c)    = [amp_circ amp_gau amp_const amp_diag];%look up table for parameter values
-                        error(ns,c) = sum((Vectorize(cormat(:,:,ns)) - Vectorize(getcorrmat(amp_circ,amp_gau,amp_const,amp_diag))).^2);
-                    end
-                end
-            end
-        end
-    end
-    
-    %collect the optima per subject
-    for ns = 1:ts
-        [~,i]       = min(error(ns,:));
-        if ~any(isnan(error(ns,:)))
-            param(:,ns) = [lut(1,i) lut(2,i) lut(3,i) lut(4,i)];
-            fit(:,:,ns) = getcorrmat(param(1,ns),param(2,ns),param(3,ns),param(4,ns));
-            %     imagesc([cormat fit cormat-fit]);colorbar;
-            %     drawnow;
-        else
-            param(:,ns) = NaN;
-            fit(:,:,ns) = NaN;
-        end
-    end
-    
-elseif strcmp(method,'gradient')
-    ts       = size(cormat,3);
-    param    = nan(4,ts);
-    fit      = nan(8,8,ts);
-    opt      = optimset('display','iter','tolfun',10^-20,'tolx',10^-20,'maxfunevals',10000,'maxiter',10000);    
-    for ns = 1:ts%run across subjects
-        Y                                      = cormat(:,:,ns);%data for this subject
-        if ~any(isnan(Y(:)))%dont dive into shit
-            cost_fun                           = @(param) sum((Y(:) - Vectorize(getcorrmat(param(1),param(2),param(3),param(4)))).^2);
-            r = range(Y(:));
-            x0                                 = rand(1,4)*r;%random initialization of parameters;
-            [param(:,ns) fval exitflag output] = fminsearch(cost_fun,x0,opt);
-            if exitflag == 0
-                keyboard
-            end
-            fit(:,:,ns)                        = getcorrmat(param(1,ns),param(2,ns),param(3,ns),param(4,ns));
-            %%
-            figure(10);
-            imagesc([cormat(:,:,ns) fit(:,:,ns) cormat(:,:,ns)-fit(:,:,ns)]);colorbar;drawnow;%pause
-        end
-    end
+if ~any(isnan(Y(:)));%dont dive into shit
+    cost_fun                           = @(param) sum((Y(:) - Vectorize(getcorrmat(param(1),param(2),param(3),param(4)))).^2);
+    r                                  = range(Y(:));
+    x0                                 = rand(1,4)*2*r-r;%random initialization of parameters;
+    [param fval exitflag output]       = fminsearch(cost_fun,x0,opt);
+    fit                                = getcorrmat(param(1),param(2),param(3),param(4));
+    %
+    if exitflag == 0;keyboard;end;%sanity check for convergence;
+    if viz
+        figure(10);imagesc([cormat fit cormat-fit]);colorbar;drawnow;%pause
+    end    
+else
+    keyboard%sanity check, this should never happen.
 end
--- getcorrmat.m
+++ getcorrmat.m
@@ -4,12 +4,19 @@ function [cmat] = getcorrmat(amp_circ,amp_gau,amp_const,amp_diag)
 % computes a covariance matrix based on circular, gaussian, constant and
 % diagonal contributions.
 
+%%
+global mu
+global sigma
 
-x    = pi/4:pi/4:2*pi;
-w    = [amp_circ*cos(x);...
-    amp_circ*sin(x);...%circular components
-    make_gaussian1d(x-pi,amp_gau*2,4,0);...%gaussian component
-    -ones(1,8)*amp_const; ...%constant component.
-    diag(ones(1,8))*amp_diag];%diagonal component
-
-cmat = (w'*w);
+%%
+x      = pi/4:pi/4:2*pi;
+x      = x(:);      
+%
+G      = make_gaussian1d(x-mu,2,sigma,0);
+B      = -ones(1,8);
+D      = diag(ones(1,8));
+%
+M      = [cos(x) sin(x) G(:) B(:) D];
+% MM     = M*inv(diag(sqrt(diag(M'*M))));%unitize all basis functions.
+%%
+cmat   = M*diag([amp_circ amp_circ amp_gau amp_const repmat(amp_diag,1,8)])*M';
