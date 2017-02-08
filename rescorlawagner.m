48f3ddaaa766144efc77a2856b1eb3d886bac464
Selim Onat <onatselim@gmail.com>
2017-02-08 20:21:31 +0100

    exponential decay parameter implemented

--- rescorlawagner.m
+++ rescorlawagner.m
@@ -1,4 +1,4 @@
- function [V_t,PE] = rescorlawagner (R,a,init)
+ function [V_t,PE] = rescorlawagner (R,a_zero,a_lambda,init)
 %function rescorlawagner	computes rescorla-wagner for alpha a
 %				and initial V(t) init
 %				mm, 03/11
@@ -17,18 +17,25 @@
 %Output:
 %       V  = VT array
 %       PE  = prediction error array
-
+viz = 0;
 %%% vars
 % R  = a(2).*R;
 V  = nan(1,numel(R)); %initial values 
 V(1)=init;
 PE = zeros(1,numel(R)); %initial values 
+a  = a_zero*2.^(-[1:numel(R)]./a_lambda);
+if viz 
+figure(1001);plot(a);drawnow;ylabel('alpha');
+end
+if a_zero > 1
+    cprintf([1 0 0 ],'a_zero > 1\n');
+end
 
 %% run over trials
 for trial=1:numel(R)
 	%PE
 	PE(trial)  = R(trial) - V(trial); % calculate prediction error	
 	%Vt
-	V(trial+1) = V(trial) + a(1) * PE(trial); % update value
+	V(trial+1) = V(trial) + a(trial) * PE(trial); % update value
 end;
 V_t=V(1:numel(R));
