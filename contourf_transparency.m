094271c65995910f0ed94f36bf7731d9976df870
onatselim <onatselim@gmail.com>
2017-04-28 13:58:47 +0200

    improved functionality

--- contourf_transparency.m
+++ contourf_transparency.m
@@ -2,9 +2,12 @@ function contourf_transparency(hContour,alpha_level);
 %hContour is the second output of contourf.
 
 
-
-hFills = hContour.FacePrims;  % array of TriangleStrip objects
-[hFills.ColorType] = deal('truecoloralpha');  % default = 'truecolor'
-for idx = 1 : numel(hFills)
-   hFills(idx).ColorData(4) = 255*alpha_level;   % default=255
+try
+    hFills = hContour.FacePrims;  % array of TriangleStrip objects
+    [hFills.ColorType] = deal('truecoloralpha');  % default = 'truecolor'
+    for idx = 1 : numel(hFills)
+        hFills(idx).ColorData(4) = 255*alpha_level;   % default=255
+    end
+catch
+    fprintf('failed transparency...\n');
 end
