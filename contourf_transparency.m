e6f318d7719d5c3006326b93336cfcaff8a86274
Selim Onat <onatselim@gmail.com>
2017-04-28 14:32:01 +0200

    possibility to enter one value

--- contourf_transparency.m
+++ contourf_transparency.m
@@ -5,6 +5,9 @@ function contourf_transparency(hContour,alpha_level);
 
 hFills = hContour.FacePrims;  % array of TriangleStrip objects
 [hFills.ColorType] = deal('truecoloralpha');  % default = 'truecolor'
+if length(alpha_level) == 1
+    alpha_level = repmat(alpha_level(:)',1,numel(hFills));
+end
 for idx = 1 : numel(hFills)
    hFills(idx).ColorData(4) = 255*alpha_level(idx);   % default=255
 end
