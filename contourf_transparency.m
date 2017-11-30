4824fd1f3956017192a4aa7a9aaf65fa5596b223
Selim Onat <onatselim@gmail.com>
2017-11-30 12:50:38 +0100

    somebody (well me\!) commited the file with conflict messages on it, repaired

--- contourf_transparency.m
+++ contourf_transparency.m
@@ -2,14 +2,6 @@ function contourf_transparency(hContour,alpha_level);
 %hContour is the second output of contourf.
 
 
-<<<<<<< HEAD
-
-hFills = hContour.FacePrims;  % array of TriangleStrip objects
-[hFills.ColorType] = deal('truecoloralpha');  % default = 'truecolor'
-for idx = 1 : numel(hFills)
-   hFills(idx).ColorData(4) = 255*alpha_level(idx);   % default=255
-end
-=======
 try
     hFills = hContour.FacePrims;  % array of TriangleStrip objects
     [hFills.ColorType] = deal('truecoloralpha');  % default = 'truecolor'
@@ -22,4 +14,3 @@ try
 catch
     fprintf('failed transparency...\n');
 end
->>>>>>> b705a5a9e1c6aee05d6af6013db5c480936c210c
