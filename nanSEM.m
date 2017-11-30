9f0529376ea03795a592693113a64b06dd1d712d
Selim Onat <onatselim@gmail.com>
2017-11-30 12:44:28 +0100

    corrected mistake in computing the number of participants

--- nanSEM.m
+++ nanSEM.m
@@ -3,6 +3,6 @@ function [data_sem]=nanSEM(data)
 %
 % Computes the SEM of matrix along the first dimension. Takes care of NaNs
 
-data_sem = squeeze(nanstd(data,1,1)./sqrt(sum(isnan(data))));
+data_sem = squeeze(nanstd(data,1,1)./sqrt(sum(~isnan(data))));
 
 
